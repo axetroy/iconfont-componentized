3ff97cff69516ace3b6b04f1ed0a6fa4ef9338ae
Axetroy <axetroy.dev@gmail.com>
2023-08-02 19:53:58 +0800

    update

--- packages/iconfont-componentized-gen-react/demo/index.jsx
+++ packages/iconfont-componentized-gen-react/demo/index.jsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import IconFont, { IconFontJia } from "../__tests__/output.spec.1";
+import IconFont, { IconFontJia, IconFontAnonymousIconfont, IconFontBriefcase, IconFontCanjuYongcan } from "../__tests__/output.spec.1";
 
 const container = document.getElementById("root");
 
@@ -8,10 +8,16 @@ const root = createRoot(container);
 root.render(
     <div>
         <div>
-            IconFont: <IconFont name="jia" style={{ width: 20, height: 20 }}></IconFont>
+            <IconFont name="jia" style={{ width: 20, height: 20 }}></IconFont>
+            <IconFont name="anonymous-iconfont" style={{ width: 20, height: 20 }}></IconFont>
+            <IconFont name="briefcase" style={{ width: 20, height: 20 }}></IconFont>
+            <IconFont name="canju-yongcan" style={{ width: 20, height: 20 }}></IconFont>
         </div>
         <div>
-            IconFontJia: <IconFontJia style={{ width: 20, height: 20 }}></IconFontJia>
+            <IconFontJia style={{ width: 20, height: 20 }}></IconFontJia>
+            <IconFontAnonymousIconfont style={{ width: 20, height: 20 }}></IconFontAnonymousIconfont>
+            <IconFontBriefcase style={{ width: 20, height: 20 }}></IconFontBriefcase>
+            <IconFontCanjuYongcan style={{ width: 20, height: 20 }}></IconFontCanjuYongcan>
         </div>
     </div>,
 );
